988b1d308d47f5dec35cf5d80e3b91b33acff610
MrCappuccino <matas234@gmail.com>
2018-12-13 21:14:18 +0100

    Made imgur script submodule, changed monitor orientation

--- bin/monitor_layout.sh
+++ bin/monitor_layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 -r 75 --primary --mode 2560x1080 --pos 1080x400 --rotate normal --output DVI-1 --auto -r 144 --mode 1920x1080 --pos 0x0 --rotate left --output DVI-0 --auto --off --output HDMI-0 --off
+xrandr --output DisplayPort-0 --primary --mode 2560x1080 --pos 1920x0 --rotate normal --output DVI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --off --output HDMI-0 --off
