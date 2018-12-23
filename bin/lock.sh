b4ecbc14f2d20b4d3536933df066bc2791975750
MrCappuccino <matas234@gmail.com>
2018-12-23 22:34:11 +0200

    Added new lock script, moved all xbindkeys binds to i3config

--- bin/lock.sh
+++ bin/lock.sh
@@ -2,11 +2,15 @@
 
 IMAGE=~/bin/lock.png
 SCREENSHOT="maim $IMAGE"
+ICON=~/bin/icon.png
 
 BLURTYPE="2x4"
 
 $SCREENSHOT
-convert $IMAGE -blur $BLURTYPE $IMAGE
+#convert $IMAGE -blur $BLURTYPE $IMAGE
+convert $IMAGE -scale 10% -scale 1000% $IMAGE
+#add overlay icon
+convert $IMAGE $ICON -gravity center -composite $IMAGE
 i3lock -i $IMAGE
 
 rm $IMAGE
