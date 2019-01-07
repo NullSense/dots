d4b6d3d50415d152417ef4dd20c4d2bff73f5345
MrCappuccino <matas234@gmail.com>
2019-01-07 22:13:51 +0100

    Switched to i3blocks, added i3blocks scripts, removed lock.png, uploaded weechat conf

--- bin/lock.sh
+++ bin/lock.sh
@@ -4,13 +4,11 @@ IMAGE=~/bin/lock.png
 SCREENSHOT="maim $IMAGE"
 ICON=~/bin/icon.png
 
-BLURTYPE="2x4"
+BLURTYPE="0x5"
 
 $SCREENSHOT
-#convert $IMAGE -blur $BLURTYPE $IMAGE
+convert $IMAGE -blur $BLURTYPE $IMAGE
 convert $IMAGE -scale 10% -scale 1000% $IMAGE
-#add overlay icon
-convert $IMAGE $ICON -gravity center -composite $IMAGE
 i3lock -i $IMAGE
 
 rm $IMAGE
