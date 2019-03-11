dc5c5dd9981be4aa9395ef0e2752fedb1076aa73
MrCappuccino <MrCappuccino@users.noreply.github.com>
2019-03-11 21:24:06 +0100

    Update shebangs, backups

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,8 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 date=$(date +%d-%m-%y/%H:%M:%S)
 
-echo -e "Backup started at \e[1;38;5;208m'$date'\e[0m"
+echo -e "==========================================="
+echo -e "===Backup started at '$date'==="
+echo -e "==========================================="
 
+echo -e "==========================================="
+echo -e "=========Starting music file backup========"
+echo -e "==========================================="
 # Backup music to HDD
 rsync -aHx --stats --numeric-ids --exclude="lost+found" --exclude='*.mkv' --exclude='*.rar' --exclude='*.nfo' --exclude='*.idx' ~/Music/ /mnt/windows/Music/
+
+echo -e "==========================================="
+echo -e "========Starting torrent file backup======="
+echo -e "==========================================="
+# Backup music to HDD
+rsync -aHx --stats --numeric-ids --exclude="lost+found" --exclude='*.mkv' --exclude='*.rar' --exclude='*.nfo' --exclude='*.idx' "/home/ongo/Downloads/torrent files/" "/mnt/windows/Downloads/torrent files"
+
+echo -e "==========================================="
+echo -e "=======Starting home directory backup======"
+echo -e "==========================================="
+rsync -aHx --stats --delete --numeric-ids --exclude="Videos" --exclude="Music" --exclude="Downloads" --exclude="lost+found" "/home/ongo/" "/mnt/windows/home_backup"
--- bin/monitor_layout.sh
+++ bin/monitor_layout.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
+#!/usr/bin/env bash
 xrandr --output DisplayPort-0 --primary --mode 2560x1080 --rate 75 --pos 1920x0 --rotate normal --output DVI-1 --mode 1920x1080 --rate 144 --pos 0x0 --rotate normal --output DVI-0 --off --output HDMI-0 --off
