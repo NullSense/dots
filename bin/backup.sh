f14f6622bf2fc36e92782fd4c30a8f8132602c3c
MrCappuccino <MrCappuccino@users.noreply.github.com>
2019-04-06 17:28:04 +0200

    Add music and torrent file backups to 3TB Toshiba drive

--- bin/backup.sh
+++ bin/backup.sh
@@ -2,6 +2,10 @@
 
 date=$(date +%y-%m-%d/%H:%M:%S)
 
+echo -e "----------------------------------------------------------------------------------------------------"
+echo -e "================1st Backup================="
+echo -e "=================1TB HDD==================="
+echo -e "Backup TO: /dev/sda1 UUID="ECCCAC3CCCABFEC8""
 echo -e "==========================================="
 echo -e "===Backup started at '$date'==="
 echo -e "==========================================="
@@ -9,16 +13,42 @@ echo -e "==========================================="
 echo -e "==========================================="
 echo -e "=========Starting music file backup========"
 echo -e "==========================================="
-# Backup music to HDD
-rsync -aHx --stats --numeric-ids --exclude="lost+found" --exclude='*.mkv' --exclude='*.rar' --exclude='*.nfo' --exclude='*.idx' ~/Music/ /mnt/windows/Music/
+# Backup music to 1TB HDD
+rsync -aHx --stats --numeric-ids --exclude="lost+found" --exclude='*.mkv' --exclude='*.rar' --exclude='*.nfo' --exclude='*.idx' "/home/ongo/Music/" "/mnt/windows/Music/"
 
 echo -e "==========================================="
 echo -e "========Starting torrent file backup======="
 echo -e "==========================================="
-# Backup torrent files to HDD
+# Backup torrent files to 1TB HDD
 rsync -aHx --stats --numeric-ids --exclude="lost+found" --exclude='*.mkv' --exclude='*.rar' --exclude='*.nfo' --exclude='*.idx' "/home/ongo/Downloads/torrent files/" "/mnt/windows/Downloads/torrent files"
 
 echo -e "==========================================="
 echo -e "=======Starting home directory backup======"
 echo -e "==========================================="
+# Backup home to 1TB HDD
 rsync -aHx --stats --delete --numeric-ids --exclude="Videos" --exclude="Music" --exclude="Downloads" --exclude="lost+found" "/home/ongo/" "/mnt/windows/home_backup"
+
+
+
+
+date=$(date +%y-%m-%d/%H:%M:%S)
+
+echo -e "================2nd Backup================="
+echo -e "=================3TB HDD==================="
+echo -e "==========================================="
+echo -e "===Backup started at '$date'==="
+echo -e "==========================================="
+echo -e "Backup TO: /dev/sdd2: UUID="fbfa2822-db95-4199-a844-d7ad77159700""
+
+
+echo -e "==========================================="
+echo -e "=========Starting music file backup========"
+echo -e "==========================================="
+# Backup music to 3TB HDD
+rsync -aHx --stats --numeric-ids --exclude="lost+found" --exclude='*.mkv' --exclude='*.rar' --exclude='*.nfo' --exclude='*.idx' "/home/ongo/Music/" "/mnt/3TB_Backup/Music/"
+
+echo -e "==========================================="
+echo -e "========Starting torrent file backup======="
+echo -e "==========================================="
+# Backup torrent files to 3TB HDD
+rsync -aHx --stats --numeric-ids --exclude="lost+found" --exclude='*.mkv' --exclude='*.rar' --exclude='*.nfo' --exclude='*.idx' "/home/ongo/Downloads/torrent files/" "/mnt/3TB_Backup/torrent files"
