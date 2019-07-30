6af5e08c3fb9e064ad7ac70bfdf7facb2cdb6374
Aerial <MrCappuccino@users.noreply.github.com>
2019-07-30 12:24:44 +0200

    Add server setup script

---
+++ bin/server_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# before starting install pip3, neovim, tmux through your package manager
+# assuming apt package manager
+sudo apt-get --assume-yes install neovim, python3-pip
+
+# nvim
+pip3 install --user neovim
+mkdir -p ~/.config/nvim/
+wget -nc https://raw.githubusercontent.com/MrCappuccino/dots/master/.config/nvim/init.vim -P ~/.config/nvim/ # nvim config
+
+# tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm # tmux plugin manager
+wget -nc https://raw.githubusercontent.com/MrCappuccino/dots/master/.tmux.conf -P ~/ # tmux config
+tmux source-file ~/.tmux.conf # source the config
+tmux # open tmux so it runs tpm
+~/.tmux/plugins/tpm/bin/install_plugins # install the plugins
+
+# some aliases
+echo "alias vim='nvim'
+echo alias dc='docker-compose'
+echo alias dcu='docker-compose up'
+echo alias dcd='docker-compose down'
+echo alias dcr='docker-compose down && docker-compose up'" > ~/.bash_aliases >/dev/null
